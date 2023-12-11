85d07d6ad3ccbf9a7cc4d7e031e0730e984982c6
Rick South <rick@natamus.com>
2023-12-11 23:58:25 +0100

    Superflat World No Slimes source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/superflatworldnoslimes/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.superflatworldnoslimes.util;
+
+public class Reference {
+	public static final String MOD_ID = "superflatworldnoslimes";
+	public static final String NAME = "Superflat World No Slimes";
+	public static final String VERSION = "3.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
