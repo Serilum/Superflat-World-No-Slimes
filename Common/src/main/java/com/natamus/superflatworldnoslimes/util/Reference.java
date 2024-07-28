3f2fcf60d8a5fe3d9d0300d8efc0a8498d9a4b33
Rick South <rick@natamus.com>
2024-07-28 07:12:46 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/superflatworldnoslimes/util/Reference.java
+++ Common/src/main/java/com/natamus/superflatworldnoslimes/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.superflatworldnoslimes.util;
 public class Reference {
 	public static final String MOD_ID = "superflatworldnoslimes";
 	public static final String NAME = "Superflat World No Slimes";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
--- Fabric/src/main/java/com/natamus/superflatworldnoslimes/ModFabric.java
+++ Fabric/src/main/java/com/natamus/superflatworldnoslimes/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.superflatworldnoslimes;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
 import com.natamus.superflatworldnoslimes.events.SlimeEvent;
 import com.natamus.superflatworldnoslimes.util.Reference;
@@ -12,6 +13,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
