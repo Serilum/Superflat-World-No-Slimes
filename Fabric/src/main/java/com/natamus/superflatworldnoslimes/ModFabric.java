8122b664fa14af8a8a10e557ab48b7cf0c27c493
Rick South <rick@natamus.com>
2026-03-26 01:25:14 +0100

    Superflat World No Slimes source code for 26.1.0.

---
+++ Fabric/src/main/java/com/natamus/superflatworldnoslimes/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.superflatworldnoslimes;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.superflatworldnoslimes.events.SlimeEvent;
+import com.natamus.superflatworldnoslimes.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveEntityEvents.PRE_ENTITY_JOIN_WORLD.register((Level world, Entity entity) -> {
+			return SlimeEvent.onWorldJoin(world, entity);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
